dfa6b7e3c25a205d606cb416820770ffdd55585d
Ivan <vanechka-nikitin-2004@mail.ru>
2024-08-04 14:13:54 +0800

    Add logic for messages

---
+++ app/crud.py
@@ -0,0 +1,24 @@
+from app.models import MessageInDB
+from app.schemas import Message
+from datetime import datetime
+import json
+
+
+async def get_messages(db, redis):
+    cached_messages = await redis.get('messages')
+    if cached_messages:
+        return json.loads(cached_messages)
+    messages = []
+    async for message in db.find():
+        messages.append(MessageInDB(**message))
+    await redis.set('messages', json.dumps([message.dict() for message in messages]))
+    return messages
+
+
+async def create_message(db, redis, message: Message):
+    message_data = message.dict()
+    message_data['timestamp'] = int(datetime.utcnow().timestamp())
+    result = await db.insert_one(message_data)
+    created_message = MessageInDB(**message_data, id=str(result.inserted_id))
+    await redis.delete('messages')
+    return created_message
